4c270fc86cbbb64cbb41d99abdde4524db19daef
Ruthvik27042002 <85207262+Ruthvik27042002@users.noreply.github.com>
2021-07-05 20:07:10 +0530

    first

--- hotel_management.c
+++ hotel_management.c
@@ -1,6 +1,79 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<conio.h>
+#include<ctype.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+
+void homepage();
+struct CustomerDetails   
+{
+	char roomnumber[10];
+	char name[20];
+	char address[25];
+	char phonenumber[15];
+	char nationality[15];	
+	char email[20];
+	char period[10];
+	char arrivaldate[10];
+}s;
+
 int main()
 {
+    int i=0;
+    char choice;
+    system("cls");   // FOR CLEARING SCREEN
+ 	printf("\t\t*****************\n");
+	printf("\t\t*                                               *\n");
+	printf("\t\t*       -----------------------------           *\n");
+	printf("\t\t*           WELCOME TO Taj Athulya              *\n");
+	printf("\t\t*       -----------------------------           *\n");
+	printf("\t\t*                                               *\n");
+	printf("\t\t*                                               *\n");
+	printf("\t\t*        OUR ADRESS:-Taj Athulya , Four star    *\n");
+	printf("\t\t*        ADRESS:23-24, Taj East Gate Rd,        *\n");
+	printf("\t\t*        Taj Nagari Phase 1, Near Shilpgram,    *\n");
+	printf("\t\t*        Telipara,Tajganj, Agra,Uttar Pradesh   *\n");
+	printf("\t\t*               282001, India                   *\n");
+	printf("\t\t*     CONTACT US:18-87454575552,035455852       *\n");
+	printf("\t\t*****************\n\n\n");
+    printf(" \n Press any key to continue:");
+	getch();
+    system("cls");
+    
+    while(1)
+    {
+        system("cls");
+        for(i=0;i<80;i++)
+		printf("-");
+		printf("\n");
+		printf("\t\t   **********  |MAIN MENU|  *********** \n");
+		for(i=0;i<80;i++)
+		printf("-");
+		printf("\n");
+		printf("\t\t Please enter your choice for menu:");
+		printf("\n\n");
+		printf(" \n Enter 1 -> Book a room");
+		printf("\n------------------------");
+		printf(" \n Enter 2 -> External RoomServices");
+		printf("\n----------------------------------");
+		printf(" \n Enter 3 -> Delete room Record");
+		printf("\n-----------------------------------");
+		printf(" \n Enter 4 -> Payment for selected Room");
+		printf("\n-----------------------------------");
+		printf(" \n Enter 5 -> Edit Record");
+		printf("\n-----------------------");
+		printf(" \n Enter 6 -> Exit");
+		printf("\n-----------------");
+		printf("\n");
+	    for(i=0;i<80;i++)
+		printf("-");
+		
+		choice=getche();
+		choice=toupper(choice);
+
+    }
+
+
     return 0;
 }
