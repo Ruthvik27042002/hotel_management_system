ca3812a3a7db8b0fe9defbc36d32f6fcaeca3c80
SubhashGupta005 <subhashgupta2520@gmail.com>
2021-07-25 22:57:42 +0530

    first

--- hotel_management.c
+++ hotel_management.c
@@ -167,8 +167,8 @@ void Booking()
 	{
 		f = fopen("add.txt", "w+");
 		system("cls");
-		printf("Please hold on while we set our database in your computer!!");
-		printf("\n Process completed press any key to continue!! ");
+		printf("Please hold on while we set our database in your computer!! \n");
+		printf("\n Process completed press any key to continue!! \n");
 		getch();
 	}
 	while (1)
@@ -188,8 +188,13 @@ void Booking()
 		scanf(" %s", s.email);
 		printf("Enter Period(\'x\'days):\n");
 		scanf("%s", &s.period);
+		person:
 		printf("Enter the number of persons for the stay in the hotel: \n");
 		scanf("%d", &s.countofpersons);
+		if (s.countofpersons > 4)
+		{
+			goto person;
+		}
 		printf("Enter Arrival date(dd\\mm\\yyyy):\n");
 		scanf("%s", &s.arrivaldate);
 		printf("\n");
